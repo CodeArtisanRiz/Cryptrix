2f400dd27c3e9479197f0093fb67bc72a011ae41
xtremist96 <riz1.xtreme@gmail.com>
2022-01-02 23:25:51 +0530

    2nd commit
    cryptoDetails
    Loader
    Charts

--- src/components/Cryptocurrencies.jsx
+++ src/components/Cryptocurrencies.jsx
@@ -3,7 +3,7 @@ import millify from 'millify';
 import { Link } from 'react-router-dom';
 import { Card, Row, Col, Input } from 'antd';
 import { useGetCryptosQuery } from '../services/cryptoApi';
-
+import Loader from './Loader';
 const Cryptocurrencies = ( {simplified} ) => {
     const count = simplified ? 10 : 100;
     const { data: cryptosList, isFetching } = useGetCryptosQuery(count);
@@ -18,7 +18,7 @@ const Cryptocurrencies = ( {simplified} ) => {
     
     // console.log(cryptos);
 
-    if(isFetching) return 'Loading..';
+ if (isFetching) return < Loader />;
     return (
         <>
             {!simplified && (
@@ -29,12 +29,13 @@ const Cryptocurrencies = ( {simplified} ) => {
 
             <Row gutter={[32, 32]} className="crypto-card-container">
                 {cryptos?.map((currency)=>(
-                    <Col xs={24} sm={12} lg={6} className="crypto-card" key={currency.id}>
-                        <Link to={`/crypto/${currency.id}`}>
+                    <Col xs={24} sm={12} lg={6} className="crypto-card">
+                        <Link key ={currency.uuid} to={`/crypto/${currency.uuid}`}>
                             <Card title={`${currency.rank}. ${currency.name}`}
                                 extra={<img className="crypto-image" src={currency.iconUrl} />}
                                 hoverable
                             >
+                                <p>id: {(currency.uuid)} </p>
                                 <p>Price: {millify(currency.price)}$ </p>
                                 <p>Market Cap: {millify(currency.marketCap)} </p>
                                 <p>Daily Change: {millify(currency.change)}%  </p>
--- src/components/Homepage.jsx
+++ src/components/Homepage.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 import millify from 'millify';
 import { Typography, Row, Col, Statistic } from 'antd';
 import { Link } from 'react-router-dom';
-
+import Loader from './Loader';
 import { useGetCryptosQuery } from '../services/cryptoApi';
 import {Cryptocurrencies, News } from '../components';
 
@@ -12,7 +12,7 @@ const Homepage = () => {
     const globalStats = data?.data?.stats;
 
     // console.log(globalStats.total);
-    if(isFetching) return 'Loading..';
+     if (isFetching) return < Loader />;
 //  console.log(globalStats.total);
     return (
         <>
